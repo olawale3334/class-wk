d3ecb5107108a92c72ebd7d38925de004f50ec3f
olawale3334 <olawale3334@gmail.com>
2025-04-15 11:29:12 +0100

    new added

--- classwork.js
+++ classwork.js
@@ -1,16 +1,31 @@
 let personalinfo = { name:'olawale',age:30}
 let contactinfo ={email:"olawale@gmail.com", phoneNumber: 9090}
 Object.assign(personalinfo,contactinfo)
-console.log(personal)
+console.log(personalinfo)
 
-// let key = Object.keys(personalinfo)
-// let value =Object.values(personalinfo)
-// console.log(value);
-// console.log(key);
 
 // task 2
    let todos = {
     
-      todo1:["code"] 
-
+      todo1:["coding"] ,
+      todo2:["read"],
+      todo3:["sleep"],
+      todo3:["eat"] 
    }
+   let key = Object.keys(todos)
+let value =Object.values(todos)
+console.log(value);
+console.log(key);
+
+if (key.includes('todo1') && todos.todo1.includes('coding')){
+    console.log("coding  eixst");
+    
+} else{
+    console.log('coding does not exist');
+    
+}
+let mode ={
+     theme: "dark",
+     notification: true
+}
+  Object.freeze(mode)

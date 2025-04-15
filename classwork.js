64efcc573ee2de46df406c3248db2bdbbd3bfd3b
olawale3334 <olawale3334@gmail.com>
2025-04-15 11:16:16 +0100

    new added

---
+++ classwork.js
@@ -0,0 +1,16 @@
+let personalinfo = { name:'olawale',age:30}
+let contactinfo ={email:"olawale@gmail.com", phoneNumber: 9090}
+Object.assign(personalinfo,contactinfo)
+console.log(personal)
+
+// let key = Object.keys(personalinfo)
+// let value =Object.values(personalinfo)
+// console.log(value);
+// console.log(key);
+
+// task 2
+   let todos = {
+    
+      todo1:["code"] 
+
+   }
